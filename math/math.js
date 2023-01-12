447de4c80fdd5753f7b60d8b5022f60fdeaa5679
Jose Eloy <joseeloymontenegro@gmail.com>
2023-01-12 16:36:22 -0600

    Terminamos Math del Cuadrado y Triangulo

--- math/math.js
+++ math/math.js
@@ -1,9 +1,16 @@
+console.group('Cuadrado')
+
 const ladoCuadrado = 5;
 const perimetroCuadrado = ladoCuadrado * 4;
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
 
-
+function calcularCuadrado(lado){
+    return{
+        perimetro: lado * 4,
+        area: lado * lado,
+    }
+}
 
 console.log({
     ladoCuadrado,
@@ -11,7 +18,9 @@ console.log({
     areaCuadrado,
 });
 
+console.groupEnd('Cuadrado')
 
+console.group('Triangulo')
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const ladoTrianguloBase = 4;
@@ -20,5 +29,12 @@ const alturaTriangulo = 5.5;
 const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
 const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
 
+function calcularTriangulo(lado1, lado2, lado3, base, altura){
+    return{
+        perimetro: (lado1 + lado2 + base),
+        area: (base * altura) / 2,
+    }
+}
 
+console.groupEnd('Triangulo')
 
