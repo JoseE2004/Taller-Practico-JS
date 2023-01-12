2ab9af9a54ed6358adaca0fb2c508f18e7662caf
Jose Eloy <joseeloymontenegro@gmail.com>
2023-01-12 16:14:31 -0600

    Insercion de matematicas

---
+++ math/math.js
@@ -0,0 +1,24 @@
+const ladoCuadrado = 5;
+const perimetroCuadrado = ladoCuadrado * 4;
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+
+
+console.log({
+    ladoCuadrado,
+    perimetroCuadrado,
+    areaCuadrado,
+});
+
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTrianguloBase = 4;
+const alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
+
+
+
